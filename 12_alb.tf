d3cf6347137d4c8d3e455c01c000f4bbfb3513ac
suuu <holic1208@daum.net>
2021-11-12 14:36:52 +0900

    step1

---
+++ 12_alb.tf
@@ -0,0 +1,11 @@
+resource "aws_lb" "suuu_alb" {
+  name = "suuu-alb"
+  internal = false
+  load_balancer_type = "application"
+  security_groups = [aws_security_group.suuu_sg.id]
+  subnets = [aws_subnet.SUUUU_puba.id,aws_subnet.SUUUU_pubb.id]
+  
+  tags = {
+    "Name"  =  "suuu_alb" 
+  }
+}
