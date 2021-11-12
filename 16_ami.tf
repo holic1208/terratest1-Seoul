e01d56e52b22caaa98deb0a236e36faa7dfa6f24
suuu <holic1208@daum.net>
2021-11-12 14:45:21 +0900

    step2

--- 16_ami.tf
+++ 16_ami.tf
@@ -1,8 +1,11 @@
 resource "aws_ami_from_instance" "suuu_ami" {
-  name = "suuu-aws.ami"
+  name               = "suuu-aws.ami"
   source_instance_id = aws_instance.suuu_weba.id
 
   depends_on = [
     aws_instance.suuu_weba
   ] # 오류 날 수도 있으니 meta argument 작업 
+  tags = {
+    "Name" = "suuu-ami"
+  }
 }
