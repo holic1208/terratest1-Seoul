25789eb9343a5ae0c9b976fda502b52bb85b5f73
suuu <holic1208@daum.net>
2021-11-12 15:10:16 +0900

    step2

--- 12_alb.tf
+++ 12_alb.tf
@@ -1,11 +1,15 @@
 resource "aws_lb" "suuu_alb" {
-  name = "suuu-alb"
-  internal = false
+  name               = "suuu-alb"
+  internal           = false
   load_balancer_type = "application"
-  security_groups = [aws_security_group.suuu_sg.id]
-  subnets = [aws_subnet.SUUUU_puba.id,aws_subnet.SUUUU_pubb.id]
-  
+  security_groups    = [aws_security_group.suuu_sg.id]
+  subnets            = [aws_subnet.SUUUU_puba.id, aws_subnet.SUUUU_pubb.id]
+
   tags = {
-    "Name"  =  "suuu_alb" 
+    "Name" = "suuu_alb"
   }
+}
+
+output "alb_dns" {
+    value = aws_lb.suuu_alb.dns_name  
 }
