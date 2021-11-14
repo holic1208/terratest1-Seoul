b5ce0319af084550d6d02d6ad26b2315c407f841
suuu <holic1208@daum.net>
2021-11-14 23:37:13 +0900

    'step2'

--- 14_albli.tf
+++ 14_albli.tf
@@ -1,10 +1,10 @@
 resource "aws_lb_listener" "suuu_albli" {
   load_balancer_arn = aws_lb.suuu_alb.arn
-  port = "80"
-  protocol = "HTTP"
-  
+  port              = "80"
+  protocol          = "HTTP"
+
   default_action {
-    type = "forward"
+    type             = "forward"
     target_group_arn = aws_lb_target_group.suuu_albtg.arn
   }
 }
