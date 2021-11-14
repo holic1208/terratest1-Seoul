ec7da3a98218e642f8066f07fc9a26b8618319b8
suuu <holic1208@daum.net>
2021-11-14 23:37:30 +0900

    'step2'

--- 17_aslc.tf
+++ 17_aslc.tf
@@ -1,6 +1,6 @@
 resource "aws_launch_configuration" "suuu_aslc" {
   name_prefix          = "suuu-web-"
-  image_id             = aws_ami_from_instance.suuu_ami.id
+  image_id             = aws_ami_from_instance.suuu_ami1.id
   instance_type        = "t2.micro"
   iam_instance_profile = "admin_role"
   security_groups      = [aws_security_group.suuu_sg.id]
